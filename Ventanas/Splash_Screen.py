4d5940f6c7f3341eddef63b3bb8b5e460aed640e
Giancarlalo <66221213+Giancarlalo@users.noreply.github.com>
2021-07-28 21:48:37 -0600

    Update Splash_Screen.py

--- Ventanas/Splash_Screen.py
+++ Ventanas/Splash_Screen.py
@@ -1,13 +1,24 @@
+from guizero import App , Text, Window, PushButton
+from time import sleep, time
 
-from tkinter.constants import LEFT
-from guizero import App , Text
-from guizero.TextBox import TextBox
+def open_window():
+    Window.show()
+
+def close_window():
+    Window.hide(wait=True)
+
+def open_window():
+    window.show(wait=True)
 app = App(title="Bienvenida",bg="light blue")
 
+window = Window(app, title = "2nd Window", height=300, width=200)
+window.hide()
 
-Text = Text(app, text="¡BIENVENIDO!", font='Serif', size=45, width="fill", align=LEFT)
+
+App.after(app,2000,open_window)
+App.after(window,2000,close_window)
+
+Text = Text(app, text="¡BIENVENIDO!", font='Serif', size=45, width="fill", height="fill")
 
 
 app.display()
-
-
